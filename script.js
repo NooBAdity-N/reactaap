7edfa36afa721c12c39b7fd56cb8272714990973
annagargoje_b23@it.vjti.ac.in <nagargoje@Adityas-MacBook-Air.local>
2025-08-10 10:43:52 +0530

    first

---
+++ script.js
@@ -0,0 +1,15 @@
+// const header1 = document.createElement('h1');
+// header1.innerText = ("Hello Coder Army");
+// header1.style.backgroundColor = "blue";
+// header1.style.fontSize = "50px";
+// header1.style.color = "pink";
+
+// const header2 = document.createElement("h1");
+// header2.innerText = "Kaise ho aap";
+// header2.style.backgroundColor = "blue";
+// header2.style.fontSize = "50px";
+// header2.style.color = "pink";
+
+
+// document.body.appendChild(header1);
+// document.body.appendChild(header2);
